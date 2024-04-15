95fdefa7927e51b82749c4e0de20e23d6cc29b29
Your Name <you@example.com>
2024-04-15 10:00:13 +0800

    improve delete bot

--- src/game/Battlegrounds/BattleGround.cpp
+++ src/game/Battlegrounds/BattleGround.cpp
@@ -1816,9 +1816,8 @@ void BattleGround::HandleCommand(Player* player, ChatHandler* handler, char* arg
 bool BattleGround::DeleteBattleBot(Team team, bool all)
 {
     bool result = false;
-    for (auto const& itr : GetPlayers())
-    {
-        Player const* pPlayertmp = sObjectMgr.GetPlayer(itr.first);
+    for (auto itr = GetPlayers().rbegin(); itr != GetPlayers().rend(); ++itr) {
+        Player const* pPlayertmp = sObjectMgr.GetPlayer(itr->first);
         if (pPlayertmp->GetTeam() == team)
         {
             if (pPlayertmp->IsBot())
